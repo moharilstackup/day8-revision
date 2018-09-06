eddad75878320f310a4748ffe8f35fd2a0249834
Mohd Shahril <moharilstackup@gmail.com>
2018-09-06 22:13:43 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category.component';
+import { CharacterComponent } from './components/character.component';
+import { CharacterDetailComponent } from './components/character-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    CharacterComponent,
+    CharacterDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
